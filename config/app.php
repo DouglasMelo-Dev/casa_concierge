82d204fc18fa936dd0ded01e0d96ab216d3bf733
Douglas Melo <douglas.melo@venturaerm.com>
2024-10-22 08:31:02 -0300

    Created: Lang

--- config/app.php
+++ config/app.php
@@ -77,13 +77,11 @@ return [
     | set to any locale for which you plan to have translation strings.
     |
     */
+    'locale' => 'pt_BR',
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'pt_BR'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
-
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
-
+    'faker_locale' => env('APP_FAKER_LOCALE', 'pt_BR'),
     /*
     |--------------------------------------------------------------------------
     | Encryption Key

2cdc83c7c9bd5edef420ccf85df4479ae81c1b21
Douglas Melo <douglas.melo@venturaerm.com>
2024-10-22 11:08:33 -0300

    Created: Colors e Layouts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,22 +3,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'App' }}</title>
+    <title>{{ $title ?? 'CasaConcierge' }}</title>
+    {{-- Master - CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/styles/master.css') }}">
+    {{-- Bootstrap - CSS --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- Google Fonts - Noto Sans--}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     @livewireStyles
+
+    <style>
+        a{
+            text-decoration: none;
+        }
+
+        body{
+            height: 100vh;
+            width: 100%;
+            margin: 0 auto;
+        }
+        .home_logo{
+            color: #BF7B50;
+        }
+
+    </style>
 </head>
+
 <body>
-    <header>
-        <div>Logo</div>
+    <header class="d-flex justify-content-between align-items-center p-3 bg-light">
+        <div>
+            <a class="home_logo fs-3" href="{{ route('home') }}">Casa<strong>Concierge</strong></a> <!-- Logo -->
+        </div>
+        @if (Route::has('login'))
+            <nav class="d-flex flex-1 justify-end">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="btn btn-primary">
+                        Dashboard
+                    </a>
+                @else
+                    <a href="{{ route('login') }}" class="btn btn_custom_enter">
+                        Entrar
+                    </a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="btn btn_custom_register ms-2">
+                            Registrar-se
+                        </a>
+                    @endif
+                @endauth
+            </nav>
+        @endif
     </header>
 
-
-
-    <div class="container">
+    <main  class="container-fluid">
         {{ $slot }}
-    </div>
-
-
+    </main>
 
     @livewireScripts
+    {{-- Bootstrap - JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,32 @@
-<div>
-    Minha
+<div class="container container_home pt-5 pb-5 d-flex justify-content-center">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+            <div class="card card_home_custom d-flex flex-column justify-content-center shadow" style="height: 200px;">
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <h5 class="card-title text-center">Airbnb - Portfolio</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card_home_custom d-flex flex-column justify-content-center shadow" style="height: 200px;">
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <h5 class="card-title text-center">Imóveis a Venda</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card_home_custom d-flex flex-column justify-content-center shadow" style="height: 200px;">
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <h5 class="card-title text-center">Gestão de Airbnb</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card_home_custom d-flex flex-column justify-content-center shadow" style="height: 200px;">
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <h5 class="card-title text-center">Gestão de Propriedade</h5>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
